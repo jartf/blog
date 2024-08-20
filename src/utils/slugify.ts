939bd3a72adf8be0d5294ab0406fcc015aeeb63a
Jarema <git@jarema.me>
2024-08-20 08:34:02 +0000

    Revert "Merge from upstream"

--- src/utils/slugify.ts
+++ src/utils/slugify.ts
@@ -1,5 +1,5 @@
-import kebabcase from "lodash.kebabcase";
+import { slug as slugger } from "github-slugger";
 
-export const slugifyStr = (str: string) => kebabcase(str);
+export const slugifyStr = (str: string) => slugger(str);
 
 export const slugifyAll = (arr: string[]) => arr.map(str => slugifyStr(str));
